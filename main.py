30b0abd44cd297409020cdecebb5f8f56926fb0f
erick <erick_093@hotmail.com>
2021-04-26 21:01:34 +0200

    Added evaluation phase

--- main.py
+++ main.py
@@ -9,14 +9,13 @@ import matplotlib.pyplot as plt
 import sklearn  # For LabelEncoder and Metrics
 from sklearn import preprocessing  # For the 🏷 Label Encoder
 import albumentations  # For Image Augmentations
+import shutil
 from sklearn.model_selection import train_test_split
 from torchvision import models, transforms
 import utils
 import torch
 from torch.utils.data import Dataset, DataLoader
 import torch.nn as nn
-import torch.optim as optim
-from albumentations.pytorch import ToTensorV2  # For Converting to torch.Tensor
 from data_loader import GetData
 import mpi_tools
 comm = MPI.COMM_WORLD
@@ -61,8 +60,20 @@ def read_json(input_file):
         print("OS error: {0}".format(err))
 
 
-def chunk_data(lst, n):
-    return [lst[i::n] for i in range(n)]
+def save_checkpoint(state, epoch, checkpoint_dir, best_model_dir, is_best=True):
+    #f_path = checkpoint_dir / 'checkpoint_{}.pt'.format(epoch)
+    f_path = checkpoint_dir + 'checkpoint.pt'
+    torch.save(state, f_path)
+    if is_best:
+        best_fpath = best_model_dir + 'best_model.pt'
+        shutil.copyfile(f_path, best_fpath)
+
+
+def load_checkpoint(checkpoint_fpath, model, optimizer):
+    checkpoint = torch.load(checkpoint_fpath)
+    model.load_state_dict(checkpoint['state_dict'])
+    optimizer.load_state_dict(checkpoint['optimizer'])
+    return model, optimizer, checkpoint['epoch']
 
 
 def create_dataframe(ann_file):
@@ -76,39 +87,64 @@ def main():
     device = 'cpu'  # don't use cuda, causes an O.S crash
     torch.set_num_threads(1)
     # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    # LOGGER.info("Device Loaded: {}".format(device))
     filenames_to_scatter = None
-    Transform = transforms.Compose(
-        [transforms.ToTensor(),
-         transforms.Resize((utils.WIDTH, utils.HEIGHT)),
-         transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))])
+    # Transform = transforms.Compose(
+    #     [transforms.ToTensor(),
+    #      transforms.Resize((utils.WIDTH, utils.HEIGHT)),
+    #      transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))])
+    Transform = transforms.Compose([
+        transforms.RandomResizedCrop(utils.WIDTH),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+         ])
     if rank == 0:
         train_input_file = utils.TRAIN_DIR + utils.TRAIN_FILE
         ann_file = read_json(train_input_file)
         LOGGER.info("JSON Train File Read")
         train_df = create_dataframe(ann_file)
         LOGGER.info("Train DataFrame Created")
+
         if utils.DEBUG:
-            folds = train_df.sample(utils.N_IMAGES, random_state=0).reset_index(drop=True).copy()
+            # val_sample, train_sample = train_test_split(train_df, test_size=0.03, stratify=train_df['category_id']).copy()  # at least n=64500 / 2,257,759
+            # test_sample = val_sample.sample(utils.N_IMAGES, random_state=0).reset_index(drop=True).copy()
+            sample = train_df.sample(utils.N_IMAGES, random_state=0).reset_index(drop=True).copy()
+            train_sample, test_sample = train_test_split(sample, test_size=0.2).copy()
         else:
-            folds = train_df.copy()
-        filenames_to_scatter = np.array_split(folds, size)
+            train_sample, test_sample = train_test_split(train_df, test_size=0.2, stratify=train_df['category_id']).copy()  # at least n=64500 / 2,257,759
+
+        filenames_to_scatter = np.array_split(train_sample, size)
 
     my_filenames = comm.scatter(filenames_to_scatter, root=0)
     LOGGER.info("_Files Received: {}".format(len(my_filenames)))
     train_set = GetData(Dir=utils.TRAIN_DIR, FNames=my_filenames['file_name'].values,
                         Labels=my_filenames['category_id'].values, Transform=Transform)
+    LOGGER.info("_Training Dataset Object Created")
     train_loader = DataLoader(train_set, batch_size=utils.BATCH_SIZE, shuffle=True)
-    LOGGER.info("_Loader Created")
+    LOGGER.info("_Training Loader Created")
+    if rank == 0 and utils.VALIDATE:
+        # val_set = GetData(Dir=utils.TRAIN_DIR, FNames=test_sample['file_name'].values,
+        #                   Labels=test_sample['category_id'].values, Transform=Transform)
+        val_set = GetData(Dir=utils.TRAIN_DIR, FNames=train_sample['file_name'].values,
+                          Labels=train_sample['category_id'].values, Transform=Transform)
+        LOGGER.info("_Validation Dataset Object Created")
+        val_loader = DataLoader(val_set, batch_size=utils.BATCH_SIZE, shuffle=False)
+        LOGGER.info("_Validation Loader Created")
+    # TODO: Add support for pretrained models from pytorch
     model = models.resnet34()
-    LOGGER.info("_Model Created")
     model.fc = nn.Linear(512, utils.NUM_CLASSES, bias=True)
-    mpi_tools.sync_params(model)
-    model.to(device)
-    LOGGER.info("_Model loaded")
-    criterion = nn.CrossEntropyLoss()
+    LOGGER.info("_Model Created")
     optimizer = torch.optim.Adam(model.parameters(), lr=utils.LR)
     LOGGER.info("_Optimizer Created")
+    if utils.CHECKPOINT:
+        LOGGER.info("_Loading Checkpoint")
+        model, optimizer, start_epoch = load_checkpoint(utils.CHECKPOINT_DIR + utils.CHECKPOINT_NAME, model, optimizer)
+        LOGGER.info("_Checkpoint loaded")
+    mpi_tools.sync_params(model)
+    model.to(device)
+    LOGGER.info("_Model loaded to CPU")
+    criterion = nn.CrossEntropyLoss()
+    LOGGER.info("_Entering training Loop")
     for epoch in range(utils.NUM_EPOCHS):
         tr_loss = 0.0
         model = model.train()
@@ -120,14 +156,38 @@ def main():
             loss = criterion(logits, labels)
             optimizer.zero_grad()
             loss.backward()
+            LOGGER.info("_Epoch: {}, sharing gradients".format(epoch))
             mpi_tools.mpi_avg_grads(model)
             optimizer.step()
             tr_loss += loss.detach().item()
-        end_time = MPI.Wtime()
-        #model.eval()
-        LOGGER.info("_Epoch: {} | Loss: {} | Time: {}".format(epoch, tr_loss/len(train_loader), end_time - init_time))
-        #print('Epoch: %d | Loss: %.4f' % (epoch, tr_loss))
 
+        end_time = MPI.Wtime()
+        LOGGER.info("_Epoch: {} | Train Loss: {} | Time: {}".format(epoch, tr_loss/len(train_loader), end_time - init_time))
+        if rank == 0: # FIXME: Move model evaluation outside the for loop, its lagging the other processes
+            checkpoint = {
+                'epoch': epoch,
+                'state_dict': model.state_dict(),
+                'optimizer': optimizer.state_dict(),
+                'loss': tr_loss/len(train_loader)
+            }
+            LOGGER.info("_Creating a checkpoint at epoch {}".format(epoch))
+            save_checkpoint(checkpoint, epoch, utils.CHECKPOINT_DIR, utils.MODELS_DIR)
+            LOGGER.info("_Checkpoint saved")
+            model.eval()
+            val_loss = 0
+            running_corrects = 0
+            #preds = np.zeros((len(test_sample)))
+            LOGGER.info("_Evaluating model")
+            for i, (images, labels) in enumerate(val_loader):
+                images = images.to(device)
+                labels = labels.to(device)
+                with torch.no_grad():
+                    outputs = model(images)
+                _, preds = torch.max(outputs, 1)
+                running_corrects += torch.sum(preds == labels.data)
+            epoch_acc = running_corrects.double() / len(val_loader.dataset)
+            LOGGER.info("_Epoch: {} | Acc: {}".format(epoch, epoch_acc))
+    # TODO: Construct the distributed prediction pipeline
 
 if __name__ == '__main__':
     main()
--- utils.py
+++ utils.py
@@ -4,22 +4,26 @@ Utils.py
 DEBUG = True
 NUM_CLASSES = 64500
 N_IMAGES = 1000
+CHECKPOINT = True
+VALIDATE = True
+CHECKPOINT_NAME = 'checkpoint.pt'
 """
 Directories
 """
 TRAIN_DIR = "D:/Huge Data Set/train/"
 TRAIN_FILE = "metadata.json"
 TEST_DIR = "D:/Huge Data Set/test/"
+CHECKPOINT_DIR = "./checkpoints/"
+MODELS_DIR = "./models/"
 """
 Image constants
 """
-WIDTH = 128
-HEIGHT = 128
-
+WIDTH = 224
+HEIGHT = 224
 """
 Training constants
 """
 BATCH_SIZE = 128
 LR = 4e-4
-NUM_EPOCHS = 2
+NUM_EPOCHS = 3
 FEATURE_EXTRACT = False
